2b3d73b41b08821b62322a5ec81c8b7e2631b225
HaopengSun <sunhaopeng@gmail.com>
2021-02-07 23:34:49 +0000

    add new map

--- planning/google-map/server.js
+++ planning/google-map/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return  Math.random().toString(36).substring(2,8);
+}
+
+const db = {
+  "userRandomID" : {id: "userRandomID", coords: {lat:42.3601, lng:-71.0589},
+                    title: 'restaurant', category: 'restaurant', description: 'restaurant'},
+  "userRandomID2": {id: "userRandomID2", coords:{lat:42.8584, lng:-70.9300},
+                    title: 'shopping mall', category: 'shopping mall', description: 'shopping mall'}
+};
 
 app.get('/', (req, res) => {
   const templateVars = {
@@ -19,10 +32,26 @@ app.get('/new-map', (req, res) => {
   res.render('new');
 });
 
-app.get('/detail', (req, res) => {
+app.post('/new-map', (req, res) => {
+  const key = generateRandomString();
+  const currentPosition = JSON.parse(req.body.position)
+  db[key] = {
+    id: key,
+    coords: currentPosition,
+    title: req.body.title,
+    category: req.body.category,
+    description: req.body.description
+  };
+  res.redirect(`/detail/${key}`);
+});
+
+app.get('/detail/:id', (req, res) => {
+  const map = db[req.params.id];
   const templateVars = {
-    position: {lat:42.3601, lng:-71.0589},
-    title: 'restaurant'
+    position: map['coords'],
+    title: map['title'],
+    description: map['description'],
+    category: map['category']
   }
   res.render('detail', templateVars);
 });
