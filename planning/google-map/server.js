48c8f1e67306fb992137f343496d88b579d3db23
HaopengSun <sunhaopeng@gmail.com>
2021-02-07 15:48:38 +0000

    refactor

--- planning/google-map/server.js
+++ planning/google-map/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const initMap = require('./helper');
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('click');
+  res.render('main');
 });
 
 app.listen(port, () => {
   console.log('Server running!')
 });
+
