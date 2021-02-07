14f8242dd4ca8d23cecf276408e83a075a90d798
HaopengSun <sunhaopeng@gmail.com>
2021-02-07 16:55:53 +0000

    conbine current position and click on the map

--- planning/google-map/server.js
+++ planning/google-map/server.js
@@ -5,10 +5,17 @@ const port = 8080;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('main');
+  res.render('new');
 });
 
 app.listen(port, () => {
   console.log('Server running!')
 });
 
+app.get('/new-map', (req, res) => {
+  res.render('new');
+});
+
+app.get('/detail', (req, res) => {
+  res.render('detail');
+});
