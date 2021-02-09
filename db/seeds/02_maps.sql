78497b9f2c919b84a8b46992ac5ae4778e01df06
Ali Bas <alibas01@gmail.com>
2021-02-08 21:28:52 -0500

    database input seeds and helper functions

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,9 +1,9 @@
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (1, 1, 'Best Kebab Restaurants', 'toronto', NULL, true);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (2, 1, 'Relatives Houses', 'toronto', NULL, false);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (3, 1, 'Ottawa Touristic Places', 'ottawa', NULL, true);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (4, 2, 'GTA Colleges', 'toronto', NULL, true);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (5, 2, 'Beaches', 'toronto', NULL, false);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (6, 2, 'Dance courses Ottawa', 'ottawa', NULL, false);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (7, 3, 'Best Restaurants', 'ottawa', NULL, true);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (8, 3, 'My friends', 'ottawa', NULL, false);
-INSERT INTO maps (id, user_id, title, city, last_updated_at, isPublic) VALUES (9, 3, 'Toronto Touristic Places', 'toronto', NULL, true);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (1, 'Best Kebab Restaurants', 'toronto', NULL, true);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (1, 'Relatives Houses', 'toronto', NULL, false);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (1, 'Ottawa Touristic Places', 'ottawa', NULL, true);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (2, 'GTA Colleges', 'toronto', NULL, true);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (2, 'Beaches', 'toronto', NULL, false);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (2, 'Dance courses Ottawa', 'ottawa', NULL, false);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (3, 'Best Restaurants', 'ottawa', NULL, true);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (3, 'My friends', 'ottawa', NULL, false);
+INSERT INTO maps (user_id, title, city, last_updated_at, isPublic) VALUES (3, 'Toronto Touristic Places', 'toronto', NULL, true);
