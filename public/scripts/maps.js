ce0aff601901b24ccf3c6fb20e2b962210e319a8
HaopengSun <sunhaopeng@gmail.com>
2021-02-12 13:56:50 +0000

    fix href link bug for no user situation

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -4,6 +4,10 @@ $(() => {
     $('.recommandation-content').empty();
     $.ajax({url: '/index_map', type: 'GET'}).done((result)=> {
       for (let map of result.maps) {
+        let href;
+        if(current_user){
+          href = `href="/detail/${map['id']}/${map['location_id']}"`;
+        }
         let favouriteItem = `
           <div class="col-md-6 grid-item">
             <div class="card flex-md-row mb-4 box-shadow h-md-250">
@@ -11,7 +15,7 @@ $(() => {
                 <strong class="d-inline-block mb-2 text-success">Public</strong>
                 <p hidden id="current_map_${map['id']}">${map['id']}</p>
                 <h3 class="mb-0">
-                  <a class="text-dark" href="/detail/${map['id']}/${map['location_id']}">${map['title']}</a>
+                  <a class="text-dark" ${href}>${map['title']}</a>
                 </h3>
                 <button type="button" class="btn btn-outline-info favourite_${map['id']}">Add to Favorites</button>
                 <p class="card-text mb-auto">${map['city']}</p>
